cbf15b3924bd96f920e82f728102dd7c72ab6f96
Joel Jalixto <139259113+Jjalixto@users.noreply.github.com>
2024-10-01 23:20:07 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,30 +1,25 @@
-
 # Datos de alumnos
 alumnos = {
     'C1': {'clave': 'password123', 'nombre': 'Renzo', 'apellido': 'Santillán'},
     'C2': {'clave': 'securePass456', 'nombre': 'Julio', 'apellido': 'Mandujano'},
-    'C3': {'clave': 'password789', 'nombre': 'Joel', 'apellido': 'Campos'}
+    'C3': {'clave': 'password789', 'nombre': 'Joel', 'apellido': 'Campos'},
     'C4': {'clave': 'password321', 'nombre': 'Pamela', 'apellido': 'Vinaza'}
+}
 
-#Diccionario de alumnos
-alumnos={
-  "C1":{"clave":"password123","nombre":"Joel","apellido":"Campos"},
-  "C2":{"clave":"securePass456","nombre":"Diego","apellido":"Chavez"},
-  "C3":{"clave":"password789","nombre":"Eros","apellido":"Ysmodes"}
+# Diccionario de cursos
+cursos = {
+    "C1": [
+        {"id": "C101", "nombreCurso": "Matemáticas", "nota": 18.5, "grado": "5to", "profesor": "Renzo Santillán", "estado": "aprobado"},
+        {"id": "C102", "nombreCurso": "Ciencias", "nota": 16.0, "grado": "5to", "profesor": "Pamela Vinaza", "estado": "aprobado"},
+        {"id": "C103", "nombreCurso": "Historia", "nota": 9.0, "grado": "5to", "profesor": "Joel Campos", "estado": "desaprobado"}
+    ],
+    "C2": [
+        {"id": "C201", "nombreCurso": "Matemáticas", "nota": 12.0, "grado": "5to", "profesor": "Renzo Santillán", "estado": "aprobado"},
+        {"id": "C202", "nombreCurso": "Historia", "nota": 14.5, "grado": "5to", "profesor": "Pamela Vinaza", "estado": "aprobado"}
+    ],
+    "C3": [
+        {"id": "C301", "nombreCurso": "Ciencias", "nota": 19.0, "grado": "5to", "profesor": "Joel Campos", "estado": "aprobado"}
+    ]
 }
-#Diccionario de cursos
-cursos={
-  "C1":[
-{"id":"C101","nombreCurso":"Matemáticas","nota":18.5,"grado":"5to","profesor":"Renzo Santillán","estado":"aprobado"},
-{"id":"C102","nombreCurso":"Ciencias","nota":16.0,"grado":"5to","profesor":"Pamela Vinazza","estado":"aprobado"},
- {"id":"C103","nombreCurso":"Historia","nota":9.0,"grado":"5to","profesor":"Joel Campos","estado":"desaprobado"}
-  ],
-  "C2":[
-    {},
-    {}
-  ],
-  "C3":[
-    {}
-  ]
-}
+
 
